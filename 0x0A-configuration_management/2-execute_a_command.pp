cffa0a5efd4cd6479c9547976338acc37f7c140f
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-01-19 21:23:01 +0100

    fix errors

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+# Puppet Manifest for Killing a Process named "killmenow"
+# Ensure all files end with a new line
+exec { 'killmenow':
+  command => '/usr/bin/pkill -f killmenow',
+  onlyif  => '/usr/bin/pgrep -f killmenow',
 }
