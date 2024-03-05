3ab4aa4db811776db4323972938a0d3979d7daf6
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-03-05 21:49:34 +0100

    fix first and seconde task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+"""Query Reddit API, print titles of 10 hot posts in subreddit"""
+
+from requests import get
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        None
+    """
+    if subreddit is None:
         print("None")
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    user_agent = {'User-Agent': 'my_bot/1.0'}
+    params = {'limit': 10}
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+
+    try:
+        response = get(url, headers=user_agent, params=params)
+        response.raise_for_status()
+
+        results = response.json()
+        posts = results.get('data', {}).get('children', [])
+
+        if posts:
+            print(f"Top 10 hot posts in r/{subreddit}:")
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+        else:
+            print("None")
+    except Exception as e:
+        print(f"An error occurred: {e}")
