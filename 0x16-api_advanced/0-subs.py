fbedc1e1f8e0a96c6f73760ac0ab3d1f78a22b5a
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-03-05 21:18:29 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My Reddit API Client"}
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for any HTTP errors
+    if response.status_code == 200:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
         return subscribers
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
+    else:
         return 0
