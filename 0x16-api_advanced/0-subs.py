686c8e6aae17ee842934db87dffb730c7544720d
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-03-05 21:43:02 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""
-Function to query subscribers on a given Reddit subreddit.
-"""
-import requests
+"""Queries the Reddit API and returns the number of subscribers"""
+
+from requests import get
+
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        int: The number of subscribers for the subreddit.
+        Returns 0 if the subreddit is invalid.
+    """
+    try:
+        user_agent = {'User-Agent': 'MyBot/1.0'}
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = get(url, headers=user_agent)
 
-    if response.status_code == 200:
-        data = response.json()
-        if 'kind' in data and data['kind'] == 't5':
-            subscribers = data['data']['subscribers']
-            return subscribers
-
-    return 0
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            print(f"Error: Unable to access subreddit '{subreddit}'")
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
