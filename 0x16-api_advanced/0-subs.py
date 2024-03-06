a558dd3837233dfacc1766d38a7f47ef26e4854e
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-03-06 12:21:02 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-Importing requests module
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
 """
-
 import requests
 
+
 def number_of_subscribers(subreddit):
-    if subreddit is None or not isinstance(subreddit, str):
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        response = requests.get(url, headers=user_agent)
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            all_data = response.json()
-            return all_data.get('data').get('subscribers')
-        else:
-            print("Error: Unable to fetch data. Status Code:", response.status_code)
-
-    except Exception as e:
-        print("Error:", str(e))
-
-    return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
